86eac4dca66b704189a9ad5b49f220b62117df2d
DR4KEN <pogoman28@gmail.com>
2017-03-20 11:55:05 +0100

    Tests de l'ajout des spinners couleurs et donnees

--- app/src/main/java/fr/lenours/sensortracker/StepGraphFragment.java
+++ app/src/main/java/fr/lenours/sensortracker/StepGraphFragment.java
@@ -1,12 +1,9 @@
 package fr.lenours.sensortracker;
 
-/**
- * Created by potte on 14/03/2017.
- */
-
-
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,20 +11,32 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.Thing;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
+import org.achartengine.chart.BarChart;
+import org.achartengine.chart.LineChart;
 import org.achartengine.chart.PointStyle;
 import org.achartengine.model.TimeSeries;
 import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 import org.json.JSONException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Calendar;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -39,6 +48,8 @@ public class StepGraphFragment extends Fragment {
     public static String TAG = "StepGraphFragment";
     private View view;
     private LinearLayout chartLyt;
+    private int valeurMax = 1000 ;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -47,25 +58,29 @@ public class StepGraphFragment extends Fragment {
     }
 
 
-    public static StepGraphFragment newInstance(String param1, String param2) {
+    public static StepGraphFragment newInstance() {
         StepGraphFragment fragment = new StepGraphFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // stepGraphFragment3.setContentView(R.layout.activity_main);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_step_graph_fragment2, container, false);
+
+
+
+        view = inflater.inflate(R.layout.fragment_step_graph_fragment, container, false);
 
         chartLyt = (LinearLayout) view.findViewById(R.id.chart);
-
         setupGraph();
         chartLyt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,50 +92,172 @@ public class StepGraphFragment extends Fragment {
         return view;
     }
 
-    private void setupGraph() {
+    protected void setupGraph() {
 
-        FileData.addConfigElement("isGraphSet",true);
-        TimeSeries series = new TimeSeries("Nombre de pas");
-
+        // Creating an XYSeries for Income
+        TimeSeries incomeSeries = new TimeSeries("Nombre de Pas");
+        // Creating an XYSeries for Expense
+        TimeSeries expenseSeries = new TimeSeries("Distance (m)");
         List<String[]> stepsDataSet = CsvReader.readCSV(FileData.TOTAL_STEP, ",");
-
         SimpleDateFormat f = new SimpleDateFormat("dd-MM-yyyy");
         Date d;
+        Calendar cal = Calendar.getInstance();
+
         if (stepsDataSet != null)
             for (String[] dayStep : stepsDataSet) {
                 try {
+
                     d = f.parse(dayStep[0]);
-                    series.add(d, Integer.parseInt(dayStep[1]));
+                    cal.setTime(d);
+                    if (Integer.parseInt(dayStep[1])>valeurMax){valeurMax=Integer.parseInt(dayStep[1]);}
+                    incomeSeries.add(cal.get(Calendar.DAY_OF_MONTH), Integer.parseInt(dayStep[1]));
+                    expenseSeries.add(cal.get(Calendar.DAY_OF_MONTH), StepTrackerFragment2.stepToMeter(Integer.parseInt(dayStep[1])));
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
             }
+        // Adding data to Income and Expense Series
 
-
+        // Creating a dataset to hold each series
         XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-        dataset.addSeries(series);
+        // Adding Income Series to the dataset
+        dataset.addSeries(incomeSeries);
+        // Adding Expense Series to dataset
+        dataset.addSeries(expenseSeries);
 
-        XYSeriesRenderer renderer = new XYSeriesRenderer();
-        renderer.setLineWidth(7);
-        renderer.setColor(Color.rgb(66, 145, 241));
-        renderer.setDisplayChartValues(true);
-        //renderer.setDisplayBoundingPoints(true);
-        renderer.setPointStyle(PointStyle.SQUARE);
-        //renderer.setPointStrokeWidth(8);
+        // Creating XYSeriesRenderer to customize incomeSeries
+        XYSeriesRenderer incomeRenderer = new XYSeriesRenderer();
+        incomeRenderer.setColor(Color.RED); // color of the graph set to cyan
+        incomeRenderer.setFillPoints(true);
+        incomeRenderer.setLineWidth(7);
+        incomeRenderer.setChartValuesTextSize(50);
+        incomeRenderer.setDisplayChartValues(true); // setting chart value
+        // distance
 
-        XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
-        mRenderer.setXLabelsColor(Color.BLACK);
-        mRenderer.setLabelsTextSize(20);
-        mRenderer.addSeriesRenderer(renderer);
-        mRenderer.setMarginsColor(Color.argb(0x00, 0xff, 0x00, 0x00)); // transparent margins
-        mRenderer.setPanEnabled(false, false);
-        mRenderer.setYAxisMax(10000);
-        mRenderer.setYAxisMin(0);
+        // Creating XYSeriesRenderer to customize expenseSeries
+        XYSeriesRenderer expenseRenderer = new XYSeriesRenderer();
+        expenseRenderer.setColor(Color.rgb(18,86,104));
+        expenseRenderer.setFillPoints(true);
+        expenseRenderer.setLineWidth(2);
+        expenseRenderer.setDisplayChartValues(true);
 
-        GraphicalView chartView = ChartFactory.getTimeChartView(getActivity(), dataset, mRenderer, "dd/MMM/yyyy");
-        chartLyt.addView(chartView);
+        // Creating a XYMultipleSeriesRenderer to customize the whole chart
+        XYMultipleSeriesRenderer multiRenderer = new XYMultipleSeriesRenderer();
+        // multiRenderer
+        //   .setOrientation(XYMultipleSeriesRenderer.Orientation.HORIZONTAL);
+        //multiRenderer.setXLabels(0);
+        multiRenderer.setChartTitle("Nombre de pas et distance parcourue quotidienne");
+        multiRenderer.setLabelsColor(Color.BLACK);
+        multiRenderer.setXTitle("Jour");
+        multiRenderer.setYTitle("Nombre de pas");
+        multiRenderer.setXLabelsColor(Color.BLACK);
+        multiRenderer.setYLabelsColor(0,Color.BLACK);
+
+
+/***
+ * Customizing graphs
+ */
+        // setting text size of the title
+        multiRenderer.setChartTitleTextSize(28);
+        // setting text size of the axis title
+        multiRenderer.setAxisTitleTextSize(30);
+        // setting text size of the graph lable
+        multiRenderer.setLabelsTextSize(30);
+        // setting zoom buttons visiblity
+        multiRenderer.setZoomButtonsVisible(false);
+        // setting pan enablity which uses graph to move on both axis
+        multiRenderer.setPanEnabled(false, false);
+        // setting click false on graph
+        multiRenderer.setClickEnabled(false);
+        // setting zoom to false on both axis
+        multiRenderer.setZoomEnabled(false, false);
+        // setting lines to display on y axis
+        multiRenderer.setShowGridY(false);
+        // setting lines to display on x axis
+        multiRenderer.setShowGridX(false);
+        // setting legend to fit the screen size
+        multiRenderer.setFitLegend(true);
+        // setting displaying line on grid
+        multiRenderer.setShowGrid(false);
+        // setting zoom to false
+        multiRenderer.setZoomEnabled(false);
+        // setting external zoom functions to false
+        multiRenderer.setExternalZoomEnabled(false);
+        // setting displaying lines on graph to be formatted(like using
+        // graphics)
+        multiRenderer.setAntialiasing(true);
+        // setting to in scroll to false
+        multiRenderer.setInScroll(false);
+        // setting to set legend height of the graph
+        multiRenderer.setLegendHeight(30);
+        multiRenderer.setLegendTextSize(30);
+        // setting x axis label align
+        multiRenderer.setXLabelsAlign(Paint.Align.CENTER);
+        // setting y axis label to align
+        multiRenderer.setYLabelsAlign(Paint.Align.LEFT);
+        // setting text style
+        multiRenderer.setTextTypeface("sans_serif", Typeface.NORMAL);
+        // setting no of values to display in y axis
+        //multiRenderer.setYLabels(10);
+        // setting y axis max value, Since i'm using static values inside the
+        // graph so i'm setting y max value to 4000.
+        // if you use dynamic values then get the max y value and set here
+        multiRenderer.setYAxisMax(valeurMax+1000);
+        multiRenderer.setYAxisMin(0);
+        // setting used to move the graph on xaxiz to .5 to the right
+        //multiRenderer.setXAxisMin(-0.5);
+        // setting max values to be display in x axis
+        //multiRenderer.setXAxisMax(10);
+        // setting bar size or space between two bars
+        multiRenderer.setBarSpacing(0.5);
+        // Setting background color of the graph to transparent
+        multiRenderer.setBackgroundColor(Color.WHITE);
+
+        multiRenderer.setApplyBackgroundColor(true);
+
+        // setting the margin size for the graph in the order top, left, bottom,
+        // right
+        multiRenderer.setMargins(new int[]{30, 30, 30, 30});
+        multiRenderer.setMarginsColor(Color.rgb(66, 145, 241));
+
+
+        /** for (int i = 0; i < x.length; i++) {
+         multiRenderer.addXTextLabel(i, mMonth[i]);
+         }**/
+
+        // Adding incomeRenderer and expenseRenderer to multipleRenderer
+        // Note: The order of adding dataseries to dataset and renderers to
+        // multipleRenderer
+        // should be same
+
+        multiRenderer.addSeriesRenderer(expenseRenderer);
+        multiRenderer.addSeriesRenderer(incomeRenderer);
+        // this part is used to display graph on the xml
+        //LinearLayout chartContainer = (LinearLayout) view.findViewById(R.id.viewpager);
+        // remove any views before u paint the chart
+        //if (chartContainer != null) {
+        //    chartContainer.removeAllViews();
+        //}
+        // drawing bar chart
+        // mChart = ChartFactory.getBarChartView(MainActivity.this, dataset,
+        // multiRenderer, Type.DEFAULT);
+        String[] types = new String[]{ BarChart.TYPE,LineChart.TYPE};
+
+        // Creating a combined chart with the chart types specified in types
+        // array
+        GraphicalView mChart = ChartFactory.getCombinedXYChartView(getActivity(), dataset,
+                multiRenderer, types);
+        // adding the view to the linearlayout
+        chartLyt.addView(mChart);
+
 
     }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
 
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -151,4 +288,6 @@ public class StepGraphFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
 }
--- app/src/main/java/fr/lenours/sensortracker/StepGraphFragment2.java
+++ app/src/main/java/fr/lenours/sensortracker/StepGraphFragment2.java
@@ -11,7 +11,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
@@ -50,11 +52,16 @@ public class StepGraphFragment2 extends Fragment {
     private LinearLayout chartLyt;
     private int valeurMax = 1000 ;
     private String[] modifications = {"Couleur", "Donnée"};
+    private String[] couleurs = {"Rouge", "Bleu"};
+    private String[] donnees = {"Pas", "Distance"};
     private String[] courbes = {"1","2","3","4","5","6"};
     private ArrayList courbesBis = new ArrayList<>();
     private Spinner nbCourbes;
     private Spinner numCourbe;
     private Spinner modification;
+    private Spinner couleurSpinner;
+    private Spinner donneeSpinner;
+    private Button valider ;
 
 
     private OnFragmentInteractionListener mListener;
@@ -83,7 +90,7 @@ public class StepGraphFragment2 extends Fragment {
                              Bundle savedInstanceState) {
 
 
-
+        valider =(Button) view.findViewById(R.id.valider);
         view = inflater.inflate(R.layout.fragment_step_graph_fragment2, container, false);
 
         chartLyt = (LinearLayout) view.findViewById(R.id.chart);
@@ -97,6 +104,8 @@ public class StepGraphFragment2 extends Fragment {
         nbCourbes = (Spinner) view.findViewById(R.id.nbCourbe);
         numCourbe = (Spinner) view.findViewById(R.id.numCourbe);
         modification = (Spinner) view.findViewById(R.id.modification);
+        couleurSpinner = (Spinner) view.findViewById(R.id.couleur);
+        donneeSpinner = (Spinner) view.findViewById(R.id.donnee);
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item,courbes);
@@ -109,8 +118,39 @@ public class StepGraphFragment2 extends Fragment {
         }
         adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item,courbes);
         numCourbe.setAdapter(adapter);
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item,couleurs);
+        couleurSpinner.setAdapter(adapter);
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item,donnees);
+        donneeSpinner.setAdapter(adapter);
         adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item,modifications);
         modification.setAdapter(adapter);
+        modification.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                }
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nombreCourbes = Integer.parseInt(nbCourbes.getSelectedItem().toString());
+                int numeroCourbes = Integer.parseInt(numCourbe.getSelectedItem().toString());
+                int choixModification = Integer.parseInt(modification.getSelectedItem().toString());
+
+            }
+        });
 
 
         return view;
